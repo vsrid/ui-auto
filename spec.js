a38b337b8eceaeebc04ea43dc26fe70a5e533484
vsrid <67600376+vsrid@users.noreply.github.com>
2020-06-30 06:54:36 -0700

    Update spec.js

--- spec.js
+++ spec.js
@@ -4,13 +4,7 @@ const { DriverProvider } = require("protractor/built/driverProviders");
 describe('check credentials', function() {
     it('login to account', function() {
     
-       browser.get('https://web501.qa.drfirst.com/login');
-       browser.driver.sleep(2000);
-      element(by.model('formData.username')).sendKeys("nlau27");
-      element(by.model('formData.password')).sendKeys("Demo2345");
-      browser.driver.sleep(2000);
-    browser.findElement(by.xpath('//*[@id="login.state"]/div/div/df-w-login/div[1]/div[2]/div[2]/div[1]/div/form/div[3]/button')).click();
-      browser.driver.sleep(8000);
+      
           
     });
 
